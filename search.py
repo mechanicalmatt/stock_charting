302550b09f6baf9c57696f31be0a5094f2ae7550
Matt Gray <matt.ta.gray@gmail.com>
2022-09-16 02:11:05 -0500

    start search fxn

---
+++ search.py
@@ -0,0 +1,42 @@
+'''
+GOAL:
+- make searchbox in streamlit
+- return close matches;
+--- if input not in list return what they may have meant based on query
+- more details in notes.txt/NOTE-2
+'''
+
+
+import streamlit as st
+
+
+options = [
+    "AAPL",
+    "APLE",
+    "AAAPL",
+    "BPLE",
+    "APL",
+    "TSLA",
+    "MSFT",
+    "SPY"
+]
+
+
+ticker = st.text_input(label="input ticker")
+
+st.write(list(ticker))
+
+st.write(ticker[:])
+
+# TODO: search for approximate matches for (ticker) in [options]
+# ---- closest match, through 5?
+
+
+
+
+
+
+
+'''
+connecting SQL db to python https://www.youtube.com/watch?v=zrNHkRgWzTI
+'''
